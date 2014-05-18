de62a61c17e95410dff8e8e3bd43acc58af21f5b
chuma <chuma@155b416f-c310-5041-93e3-512504897193>
2014-05-18 14:54:59 +0000

    .
    git-svn-id: file:///C/Home/chuma/Data/BackUp/.svnRepo/trunk/HomeAutomation@95 155b416f-c310-5041-93e3-512504897193

--- RaspberryPI/usr/local/bin/homeAutomation/runJar-debug.sh
+++ RaspberryPI/usr/local/bin/homeAutomation/runJar-debug.sh
@@ -4,7 +4,8 @@ sudo ln -s /dev/ttyAMA0 /dev/ttyS80
 cd $(dirname $0)
 ls >out/ls.txt
 
-echo Before>>out/ls.txt
-echo Before2>>out/ls.txt
+echo Before>out/ls1.txt
+echo Before2>>out/ls1.txt
 exec authbind --deep java -Xms16m -Xmx32m -Djava.library.path=/usr/lib/jni -cp ./homeAutomation.jar:/usr/share/java/RXTXcomm.jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=6070 Main
-echo After>>out/ls.txt
+echo After>>out/ls1b.txt
+
