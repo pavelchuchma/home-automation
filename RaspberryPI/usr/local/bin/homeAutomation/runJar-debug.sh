518deb6a1538d2632d53215d42ca0b0491514cff
chuma <chuma@155b416f-c310-5041-93e3-512504897193>
2014-05-23 22:06:53 +0000

    log rolling, ready for restart by cron
    git-svn-id: file:///C/Home/chuma/Data/BackUp/.svnRepo/trunk/HomeAutomation@96 155b416f-c310-5041-93e3-512504897193

--- RaspberryPI/usr/local/bin/homeAutomation/runJar-debug.sh
+++ RaspberryPI/usr/local/bin/homeAutomation/runJar-debug.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 sudo ln -s /dev/ttyAMA0 /dev/ttyS80
-cd $(dirname $0)
-ls >out/ls.txt
 
-echo Before>out/ls1.txt
-echo Before2>>out/ls1.txt
+cd $(dirname $0)
+prefix=x$(date +%Y%m%d-%H%M%S)
+cp ./out/app.log ./out/$prefix-app.log
+cp ./out/messages.log ./out/$prefix-messages.log
+
 exec authbind --deep java -Xms16m -Xmx32m -Djava.library.path=/usr/lib/jni -cp ./homeAutomation.jar:/usr/share/java/RXTXcomm.jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=6070 Main
-echo After>>out/ls1b.txt
 
