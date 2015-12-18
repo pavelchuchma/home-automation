baa4e62d4f20164de7b1d10fc1b0a3d06ea206bb
chuma <chuma@155b416f-c310-5041-93e3-512504897193>
2015-12-18 22:53:57 +0000

    cleanup
    git-svn-id: file:///C/Home/chuma/Data/BackUp/.svnRepo/trunk/HomeAutomation@165 155b416f-c310-5041-93e3-512504897193

--- controler/script/a.bat
+++
@@ -1,3 +0,0 @@
-:: putty.exe -ssh -2 -i N:\Data\cert\sshKey\sshKey.private.ppk -m test.command pi
-:: putty.exe -ssh -2 -i N:\Data\cert\sshKey\sshKey.private.ppk -l pi -m test.command pi
-putty.exe -ssh -2 -i N:\Data\cert\sshKey\sshKey.private.ppk -l pi -m restart.sh pi
--- controler/script/test.command
+++
@@ -1,1 +0,0 @@
-echo `date`;sleep 3;
