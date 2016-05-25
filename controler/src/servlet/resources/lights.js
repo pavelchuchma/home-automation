e7fd154c747cc4cb260b7bd88a1eb77d055256d3
chuma <chuma@155b416f-c310-5041-93e3-512504897193>
2016-05-25 16:14:26 +0000

    adding 2nd floor plan to console
    git-svn-id: file:///C/Home/chuma/Data/BackUp/.svnRepo/trunk/HomeAutomation@185 155b416f-c310-5041-93e3-512504897193

--- controler/src/servlet/resources/lights.js
+++ controler/src/servlet/resources/lights.js
@@ -1,63 +1,96 @@
 var mainCtx;
 var toolsCtx;
 var e = '';
-var tmp = "";
+
+var currentFloor = 0;
+var floorIds = ['1stFloor', '2ndFloor']
 
 const toolBoxBackground = 'lightgray';
 const toolLightPlusValue = 66;
 
 var itemCoordinates = [
-    //id, x, y
-    ['pwmKch1', 410, 759],
-    ['pwmKch2', 382, 708],
-    ['pwmKch3', 331, 733],
-    ['pwmKch4', 268, 686],
-    ['pwmKch5', 285, 808],
-    ["pwmJid1", 250, 880],
-    ["pwmJid2", 142, 896],
-    ["pwmJid3", 205, 962],
-    ['pwmOb1', 375, 1338],
-    ['pwmOb2', 337, 1303],
-    ['pwmOb3', 402, 1269],
-    ['pwmOb4', 380, 1199],
-    ['pwmOb5', 427, 1112],
-    ['pwmOb6', 390, 1037],
-    ['pwmOb7', 260, 1340],
-    ['pwmOb8', 236, 1269],
-    ['pwmOb9', 217, 1217],
-    ['pwmOb10', 160, 1135],
-    ['pwmOb11', 270, 1157],
-    ['pwmOb12', 185, 1276],
-    ['pwmOb13', 189, 1327],
-    ['pwmPrd1', 407, 408],
-    ['pwmPrd2', 361, 158],
-    ['pwmZadD', 390, 544],
-    ['pwmChoD', 266, 517],
-    ['pwmKpD', 153, 489],
-    ['pwmKpDZrc', 189, 544],
-    //['spajz', 206, 328],
-    //['sklepL', 518, 345],
-    //['sklepP', 567, 350],
-    ['pwmG1', 65, 150],
-    ['pwmG2', 45, 210],
-    ['pwmG3', 25, 270],
+    //id, x, y, floor
+    ['pwmKch1', 410, 759, 0],
+    ['pwmKch2', 382, 708, 0],
+    ['pwmKch3', 331, 733, 0],
+    ['pwmKch4', 268, 686, 0],
+    ['pwmKch5', 285, 808, 0],
+    ["pwmJid1", 250, 880, 0],
+    ["pwmJid2", 142, 896, 0],
+    ["pwmJid3", 205, 962, 0],
+    ['pwmOb1', 375, 1338, 0],
+    ['pwmOb2', 337, 1303, 0],
+    ['pwmOb3', 402, 1269, 0],
+    ['pwmOb4', 380, 1199, 0],
+    ['pwmOb5', 427, 1112, 0],
+    ['pwmOb6', 390, 1037, 0],
+    ['pwmOb7', 260, 1340, 0],
+    ['pwmOb8', 236, 1269, 0],
+    ['pwmOb9', 217, 1217, 0],
+    ['pwmOb10', 160, 1135, 0],
+    ['pwmOb11', 270, 1157, 0],
+    ['pwmOb12', 185, 1276, 0],
+    ['pwmOb13', 189, 1327, 0],
+    ['pwmPrd1', 407, 408, 0],
+    ['pwmPrd2', 361, 158, 0],
+    ['pwmZadD', 390, 544, 0],
+    ['pwmChoD', 266, 517, 0],
+    ['pwmKpD', 153, 489, 0],
+    ['pwmKpDZrc', 189, 544, 0],
+    //['spajz', 206, 328, 0],
+    //['sklepL', 518, 345, 0],
+    //['sklepP', 567, 350, 0],
+    ['pwmG1', 65, 150, 0],
+    ['pwmG2', 45, 210, 0],
+    ['pwmG3', 25, 270, 0],
 
-    ['lvKuch', 510, 809],
-    ['lvOb1', 510, 978],
-    ['lvOb2', 510, 1121],
-    ['lvOb3', 510, 1285],
-    ['lvOb4', 360, 1430],
-    ['lvOb5', 65, 1286],
-    ['lvOb6', 65, 882],
-    ['lvKoupD', 65, 510]
+    ['lvKuch', 510, 809, 0],
+    ['lvOb1', 510, 978, 0],
+    ['lvOb2', 510, 1121, 0],
+    ['lvOb3', 510, 1285, 0],
+    ['lvOb4', 360, 1430, 0],
+    ['lvOb5', 65, 1286, 0],
+    ['lvOb6', 65, 882, 0],
+    ['lvKoupD', 65, 510, 0],
+
+    ['stairsUp', 127, 690, 0],
+    ['stairsDown', 127, 690, 1],
+
+    ['pwmVchH', 167, 97, 1],
+    ['pwmVrt1', 342, 207, 1],
+    ['pwmVrt2', 436, 207, 1],
+    ['pwmZadH', 196, 289, 1],
+    ['pwmChSch', 137, 564, 1],
+    ['pwmChP', 208, 564, 1],
+    ['pwmKpH', 373, 446, 1],
+    ['pwmKpHZrc', 349, 516, 1],
+    ['pwmKry', 349, 636, 1],
+    ['pwmPata', 349, 830, 1],
+    ['pwmMarek', 349, 1039, 1],
+    ['pwmLozM', 309, 1280, 1],
+    ['pwmLozV',  431, 1280, 1],
+    ['pwmPrac', 167, 1324, 1],
+    ['pwmSat', 194, 1128, 1],
+    ['pwmWc', 134, 953, 1],
+
+    ['lvVrt1', 260, 118, 1],
+    ['lvVrt2', 370, 50, 1],
+    ['lvVrt3', 530, 298, 1],
+    ['lvKoupH', 530, 473, 1],
+    ['lvKrys', 530, 640, 1],
+    ['lvPata', 530, 823, 1],
+    ['lvMarek', 530, 1006, 1],
+    ['lvLoz1', 530, 1311, 1],
+    ['lvLoz2', 397, 1440, 1],
+    ['lvPrc', 55, 1325, 1],
+    ['lvSat', 55, 1085, 1],
+    ['lvCh1', 55, 850, 1],
+    ['lvCh2', 55, 512, 1]
 ];
 
 var itemStatusMap = {};
+// x, y, floor
 var itemCoordinateMap = {};
-
-var louversCoordinates = [
-    //id, x, y
-];
 
 function drawLightToolSign(x, y, ctx, drawVertical) {
     ctx.beginPath();
@@ -75,34 +108,34 @@ function drawLightToolSign(x, y, ctx, drawVertical) {
 }
 
 var toolsCoordinates = [
-    //id, x, y, draw function, prefixValidator
-    ['lightToggle', 50, 50, function (x, y, ctx) {
+    //id, x, y, floor, draw function, prefixValidator
+    ['lightToggle', 50, 50, -1, function (x, y, ctx) {
         drawLightIcon(x - 10, y, 0, ctx);
         drawLightIcon(x + 10, y, .75, ctx);
-    }, [isPwmId]],
-    ['lightPlus', 50, 150, function (x, y, ctx) {
+    }, [isPwmId, isStairs]],
+    ['lightPlus', 50, 150, -1, function (x, y, ctx) {
         drawLightIcon(x, y, toolLightPlusValue / 100, ctx);
         drawLightToolSign(x, y, ctx, true);
-    }, [isPwmId]],
-    ['lightMinus', 50, 250, function (x, y, ctx) {
+    }, [isPwmId, isStairs]],
+    ['lightMinus', 50, 250, -1, function (x, y, ctx) {
         drawLightIcon(x, y, .25, ctx);
         drawLightToolSign(x, y, ctx, false);
-    }, [isPwmId]],
-    ['lightFull', 50, 350, function (x, y, ctx) {
+    }, [isPwmId, isStairs]],
+    ['lightFull', 50, 350, -1, function (x, y, ctx) {
         drawLightIcon(x, y, 1, ctx);
-    }, [isPwmId]],
-    ['lightOff', 50, 450, function (x, y, ctx) {
+    }, [isPwmId, isStairs]],
+    ['lightOff', 50, 450, -1, function (x, y, ctx) {
         drawLightIcon(x, y, 0, ctx);
-    }, [isPwmId]],
-    ['louversUp', 50, 550, function (x, y, ctx) {
+    }, [isPwmId, isStairs]],
+    ['louversUp', 50, 550, -1, function (x, y, ctx) {
         drawLouversToolIcon(x, y, .3, 0, 'stopped', ctx);
-    }, [isLouversId]],
-    ['louversOutshine', 50, 650, function (x, y, ctx) {
+    }, [isLouversId, isStairs]],
+    ['louversOutshine', 50, 650, -1, function (x, y, ctx) {
         drawLouversToolIcon(x, y, 1, 0, 'stopped', ctx);
-    }, [isLouversId]],
-    ['louversDown', 50, 750, function (x, y, ctx) {
+    }, [isLouversId, isStairs]],
+    ['louversDown', 50, 750, -1, function (x, y, ctx) {
         drawLouversToolIcon(x, y, 1, 1, 'stopped', ctx);
-    }, [isLouversId]]
+    }, [isLouversId, isStairs]]
 ];
 
 
@@ -162,6 +195,34 @@ function drawLouversIconImpl(x, y, position, offset, action, ctx, w, h) {
     }
 }
 
+function drawCharacterIcon(id, text) {
+    var coords = itemCoordinateMap[id];
+    var x = coords[0];
+    var y = coords[1];
+
+    var w = 70;
+    var h = 80;
+
+    // white rectangle
+    mainCtx.beginPath();
+    mainCtx.rect(x - w / 2, y - h / 2, w, h);
+    mainCtx.fillStyle = 'white';
+    mainCtx.fill();
+    mainCtx.strokeStyle = 'black';
+    mainCtx.lineWidth = 2;
+    mainCtx.stroke();
+
+
+    mainCtx.beginPath();
+    mainCtx.font = h - 20  + "px Arial Bold";
+    mainCtx.fillStyle = 'black';
+    mainCtx.textAlign = "center";
+    mainCtx.textBaseline = 'middle';
+    mainCtx.fillText(text, x, y);
+    mainCtx.stroke();
+
+}
+
 var toolCoordinateMap = {};
 var selectedToolId = toolsCoordinates[0][0];
 
@@ -169,11 +230,11 @@ var selectedToolId = toolsCoordinates[0][0];
 window.onload = function () {
     try {
         itemCoordinates.forEach(function (lc) {
-            itemCoordinateMap[lc[0]] = [lc[1], lc[2]];
+            itemCoordinateMap[lc[0]] = [lc[1], lc[2], lc[3]];
         });
 
         toolsCoordinates.forEach(function (tc) {
-            toolCoordinateMap[tc[0]] = [tc[1], tc[2], tc[4]];
+            toolCoordinateMap[tc[0]] = [tc[1], tc[2], tc[5]];
         });
 
         drawMainCanvas();
@@ -203,7 +264,8 @@ function findNearestItem(x, y, coordinates, validatorArray) {
     coordinates.forEach(function (lc) {
         // validate item type
         validatorArray.some(function (validator) {
-            if (validator(lc[0])) {
+            var floor = lc[3];
+            if ((floor < 0 || floor == currentFloor) && validator(lc[0])) {
                 var dist = computeDistance(x, y, lc[1], lc[2]);
                 if (resDist < 0 || dist < resDist) {
                     resDist = dist;
@@ -215,6 +277,10 @@ function findNearestItem(x, y, coordinates, validatorArray) {
         });
     });
     return resId;
+}
+
+function isStairs(id) {
+    return startsWith(id, "stairs");
 }
 
 function isPwmId(id) {
@@ -232,10 +298,16 @@ function isLouversId(id) {
 function drawItems() {
     itemCoordinates.forEach(function (lc) {
         var id = lc[0];
-        if (isPwmId(id)) {
-            drawLight(id)
-        } else if (isLouversId(id)) {
-            drawOneLouvers(id)
+        if (lc[3] == currentFloor) {
+            if (isPwmId(id)) {
+                drawLight(id)
+            } else if (isLouversId(id)) {
+                drawOneLouvers(id)
+            } else if (id == 'stairsUp') {
+                drawCharacterIcon(id, '▲')
+            } else if (id == 'stairsDown') {
+                drawCharacterIcon(id, '▼')
+            }
         }
     });
 }
@@ -243,7 +315,7 @@ function drawItems() {
 function drawMainCanvas() {
     var c = document.getElementById("mainCanvas");
     mainCtx = c.getContext("2d");
-    var img = document.getElementById("background");
+    var img = document.getElementById(floorIds[currentFloor]);
     mainCtx.drawImage(img, 0, 0, img.width, img.height);
 
     //document.getElementById('error').innerHTML = 'LOADED!';
@@ -269,7 +341,7 @@ function drawToolsCanvas() {
     toolsCtx.stroke();
 
     toolsCoordinates.forEach(function (c) {
-        c[3](c[1], c[2], toolsCtx);
+        c[4](c[1], c[2], toolsCtx);
     });
 
     drawToolSelection();
@@ -383,8 +455,9 @@ function buildLouversActionLink(itemStatus, possiton, offset) {
     return '/louvers/action?id=' + itemStatus.id + '&pos=' + possiton + '&off=' + offset;
 }
 
+var tmp = '';
 function onCanvasClick(event) {
-    //tmp += "['pwm', " + event.offsetX + ", " + event.offsetY + "]<br>";
+    //tmp += "['pwm', " + Math.round(parseFloat(event.offsetX)) + ", " + Math.round(parseFloat(event.offsetY)) + ", 0]<br>";
     //debug(tmp);
     //return;
 
@@ -392,6 +465,13 @@ function onCanvasClick(event) {
 
     var itemId = findNearestItem(event.offsetX, event.offsetY, itemCoordinates, selectedTool[2]);
     var itemStatus = itemStatusMap[itemId];
+
+    if (isStairs(itemId)) {
+        currentFloor = (itemId == 'stairsUp') ? 1 : 0;
+        drawMainCanvas();
+        onTimer();
+        return;
+    }
 
     var action;
     if (startsWith(selectedToolId, 'light')) {
