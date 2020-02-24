e42e07eb3db397611c1fa2e2ccddac6ae87ab00a
chuma <chuma@155b416f-c310-5041-93e3-512504897193>
2020-02-24 21:37:26 +0000

    adding missing files
    git-svn-id: file:///C/Home/chuma/Data/BackUp/.svnRepo/trunk/HomeAutomation@251 155b416f-c310-5041-93e3-512504897193

---
+++ controler/script/deploy-pi.bat
@@ -0,0 +1,5 @@
+set host=pi
+pscp -2 -i N:\Data\cert\sshKey\sshKey.private.ppk %~dp0..\out\homeAutomation.jar pi@%host%:/usr/local/bin/homeAutomation/
+@if errorlevel 1 pause
+putty.exe -ssh -2 -i N:\Data\cert\sshKey\sshKey.private.ppk -l pi -m restart.sh %host%
+@if errorlevel 1 pause
