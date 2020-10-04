365f76dc969fff3e447941cb0391beacaa3eda08
Pavel Chuchma <pavel.chuchma@microfocus.com>
2020-10-04 22:39:36 +0200

    tuned scripts

--- controler/script/runJar-debug.sh
+++ controler/script/runJar-debug.sh
@@ -1,1 +1,14 @@
-#! /bin/shsudo ln -s /dev/ttyAMA0 /dev/ttyS80ls >out/ls.txtcd $(dirname $0)exec authbind --deep java -Xms16m -Xmx32m -Djava.library.path=/usr/lib/jni -cp ./homeAutomation.jar:/usr/share/java/RXTXcomm.jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=6070 Main
+#! /bin/bash
+
+sudo ln -s /dev/ttyAMA0 /dev/ttyS80
+
+cd $(dirname $0)
+prefix=x$(date +%Y%m%d-%H%M%S)
+cp ./out/app.log ./out/$prefix-app.log
+cp ./out/messages.log ./out/$prefix-messages.log
+
+#javaOptions="-XX:+UnlockCommercialFeatures -XX:+FlightRecorder -Djava.rmi.server.hostname=10.10.0.150 -Dcom.sun.management.jmxremote.port=6077 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
+
+exec authbind --deep java -Xms16m -Xmx32m $javaOptions -Djava.library.path=/usr/lib/jni -cp ./homeAutomation.jar:/usr/share/java/RXTXcomm.jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=6070 Main
+
+
