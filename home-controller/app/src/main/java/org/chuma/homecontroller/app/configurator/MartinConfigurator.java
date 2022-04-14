0572dbd31a75738ea18c45612b6e2764b31c54aa
Martin Frydl <mfrydl@gmail.com>
2022-04-14 18:54:37 +0200

    train test - just VERY simple simulator-like web controller for connector

--- home-controller/app/src/main/java/org/chuma/homecontroller/app/configurator/MartinConfigurator.java
+++ home-controller/app/src/main/java/org/chuma/homecontroller/app/configurator/MartinConfigurator.java
@@ -1,16 +1,32 @@
 package org.chuma.homecontroller.app.configurator;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import org.chuma.homecontroller.app.servlet.Handler;
+import org.chuma.homecontroller.app.servlet.Servlet;
+import org.chuma.homecontroller.app.servlet.ServletAction;
+import org.chuma.homecontroller.app.servlet.pages.NodeInfoPage;
+import org.chuma.homecontroller.app.servlet.pages.Page;
+import org.chuma.homecontroller.app.servlet.pages.StaticPage;
+import org.chuma.homecontroller.app.servlet.pages.SystemPage;
+import org.chuma.homecontroller.app.servlet.pages.TrainPage;
+import org.chuma.homecontroller.app.servlet.pages.TrainWebSocketHandler;
+import org.chuma.homecontroller.app.servlet.rest.NodeHandler;
+import org.chuma.homecontroller.app.servlet.ws.WebSocketHandler;
 import org.chuma.homecontroller.base.node.Node;
 import org.chuma.homecontroller.controller.action.Action;
 import org.chuma.homecontroller.controller.controller.LouversController;
 import org.chuma.homecontroller.controller.controller.LouversControllerImpl;
-import org.chuma.homecontroller.controller.controller.ValveController;
+import org.chuma.homecontroller.controller.device.AbstractConnectedDevice;
+import org.chuma.homecontroller.controller.device.InputDevice;
+import org.chuma.homecontroller.controller.device.OutputDevice;
 import org.chuma.homecontroller.controller.device.RelayBoardDevice;
 import org.chuma.homecontroller.controller.device.WallSwitch;
 import org.chuma.homecontroller.controller.nodeinfo.NodeInfoRegistry;
 import org.chuma.homecontroller.controller.nodeinfo.NodeListener;
+
+import static org.chuma.homecontroller.app.servlet.pages.AbstractPage.VIRTUAL_CONFIGURATION_JS_FILENAME;
 
 public class MartinConfigurator extends AbstractConfigurator {
 
@@ -24,44 +40,77 @@ public class MartinConfigurator extends AbstractConfigurator {
         ArrayList<Action> lightsActions = new ArrayList<>();
 
         Node bridge = nodeInfoRegistry.createNode(1, "Bridge");
-        Node node36 = nodeInfoRegistry.createNode(36, "Node36");
+//        Node node36 = nodeInfoRegistry.createNode(36, "Node36");
         Node node37 = nodeInfoRegistry.createNode(37, "Node37");
-        Node node40 = nodeInfoRegistry.createNode(40, "Node40");
+//        Node node40 = nodeInfoRegistry.createNode(40, "Node40");
 
-        WallSwitch switchASw = new WallSwitch("switchASw", node36, 1);
-        WallSwitch switchBSw = new WallSwitch("switchBSw", node36, 2);
+//        WallSwitch switchASw = new WallSwitch("switchASw", node36, 1);
+//        WallSwitch switchBSw = new WallSwitch("switchBSw", node36, 2);
         //WallSwitch switchCSw = new WallSwitch("switchCSw", node36, 3);
 
         // Rele51
-        RelayBoardDevice rele51 = new RelayBoardDevice("rele51", node37, 1);
+//        RelayBoardDevice rele51 = new RelayBoardDevice("rele51", node37, 1);
+        // Simple output device
+//        OutputDevice outputOnly = new OutputDevice("OutChecker", node37, 1) {
+//            public int getInitialOutputValues() { return 0; }
+//        };
+        if (false) {
+        // Train device
+        InputDevice inputOnly = new InputDevice("InputChecker", node37, 3);
+        // Activate all pins
+        inputOnly.getIn1AndActivate();
+        inputOnly.getIn2AndActivate();
+        inputOnly.getIn3AndActivate();
+        inputOnly.getIn4AndActivate();
+        inputOnly.getIn5AndActivate();
+        inputOnly.getIn6AndActivate();
+        }
+
+        new AbstractConnectedDevice("TrainControl", node37, 1, new String[] { "speed", "pass", "dir1", "-", "dir2", "-" }) {
+            @Override
+            public int getInitialOutputValues() {
+                return 0;
+            }
+
+            @Override
+            public int getEventMask() {
+                return createMask(pins[1]);
+            }
+
+            @Override
+            public int getOutputMasks() {
+                return createMask(pins[0], pins[2], pins[4], /* added also remaining pins - we don't want them input */ pins[3], pins[5]);
+            }
+        };
 
         // (vyhybky)
-        LouversController vyhybka01;
-        LouversController vyhybka02;
-        LouversController vyhybka03;
-
-        LouversController[] louversControllers = new LouversController[]{
-                vyhybka01 = new LouversControllerImpl("lvVyh01", "Vyhybka 01", rele51.getRelay1(), rele51.getRelay2(), 1000, 10),
-                vyhybka02 = new LouversControllerImpl("lvVyh02", "Vyhybka 02", rele51.getRelay3(), rele51.getRelay4(), 1000, 10),
-                vyhybka03 = new LouversControllerImpl("lvVyh03", "Vyhybka 03", rele51.getRelay5(), rele51.getRelay6(), 1000, 10),
-        };
-
-
-        configureLouvers(lst, switchASw, WallSwitch.Side.LEFT, vyhybka01);
-        configureLouvers(lst, switchASw, WallSwitch.Side.RIGHT, vyhybka02);
-        configureLouvers(lst, switchBSw, WallSwitch.Side.LEFT, vyhybka03);
+//        LouversController vyhybka01;
+//        LouversController vyhybka02;
+//        LouversController vyhybka03;
+//
+//        LouversController[] louversControllers = new LouversController[]{
+//                vyhybka01 = new LouversControllerImpl("lvVyh01", "Vyhybka 01", rele51.getRelay1(), rele51.getRelay2(), 1000, 10),
+//                vyhybka02 = new LouversControllerImpl("lvVyh02", "Vyhybka 02", rele51.getRelay3(), rele51.getRelay4(), 1000, 10),
+//                vyhybka03 = new LouversControllerImpl("lvVyh03", "Vyhybka 03", rele51.getRelay5(), rele51.getRelay6(), 1000, 10),
+//        };
 
 
-        // Air Valves
-//        ValveController vzduchVratnice;
+//        configureLouvers(lst, switchASw, WallSwitch.Side.LEFT, vyhybka01);
+//        configureLouvers(lst, switchASw, WallSwitch.Side.RIGHT, vyhybka02);
+//        configureLouvers(lst, switchBSw, WallSwitch.Side.LEFT, vyhybka03);
 
-        ValveController[] valveControllers = new ValveController[]{
-//                vzduchVratnice = new ValveControllerImpl("vlVrt", "Vratnice", rele51.getRele5(), rele51.getRele6(), 10000),
-        };
 
-//        Servlet.setLouversControllers(louversControllers);
-//        Servlet.setValveControllers(valveControllers);
-//        Servlet.setLightActions(lightsActions.toArray(new Action[lightsActions.size()]));
-//        Servlet.pirStatusList = pirStatusList;
+        List<WebSocketHandler> wsHandlers = new ArrayList<>();
+        wsHandlers.add(new TrainWebSocketHandler(nodeInfoRegistry));
+        List<ServletAction> rootActions = new ArrayList<>();
+        List<Page> pages = new ArrayList<>();
+        pages.add(new TrainPage(nodeInfoRegistry, pages));
+        pages.add(new NodeInfoPage(nodeInfoRegistry, pages, rootActions));
+        pages.add(new SystemPage(nodeInfoRegistry, pages));
+        configureSimulator(pages, wsHandlers, true);
+        List<Handler> handlers = new ArrayList<>(pages);
+        handlers.add(new StaticPage(VIRTUAL_CONFIGURATION_JS_FILENAME, "/configuration-martin.js", null));
+        handlers.add(new NodeHandler(nodeInfoRegistry));
+        servlet = new Servlet(handlers, "/system", wsHandlers);
     }
 }
---
+++ home-controller/app/src/main/java/org/chuma/homecontroller/app/servlet/pages/TrainWebSocketHandler.java
@@ -0,0 +1,127 @@
+package org.chuma.homecontroller.app.servlet.pages;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Consumer;
+
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.WebSocketAdapter;
+
+import org.chuma.homecontroller.app.servlet.rest.impl.JsonWriter;
+import org.chuma.homecontroller.app.servlet.ws.AbstractWebSocketAdapter;
+import org.chuma.homecontroller.app.servlet.ws.AbstractWebSocketHandler;
+import org.chuma.homecontroller.base.node.Node;
+import org.chuma.homecontroller.base.node.Pin;
+import org.chuma.homecontroller.controller.nodeinfo.NodeInfo;
+import org.chuma.homecontroller.controller.nodeinfo.NodeInfoRegistry;
+
+public class TrainWebSocketHandler extends AbstractWebSocketHandler implements Node.Listener {
+    private NodeInfoRegistry nodeInfoRegistry;
+    // Connected clients
+    private Set<Adapter> clients = ConcurrentHashMap.newKeySet();
+
+    public TrainWebSocketHandler(NodeInfoRegistry nodeInfoRegistry) {
+        super("/train");
+        this.nodeInfoRegistry = nodeInfoRegistry;
+        for (NodeInfo nodeInfo : nodeInfoRegistry.getNodeInfos()) {
+            nodeInfo.getNode().addListener(this);
+        }
+    }
+
+    private void processEvent(Consumer<Adapter> action) {
+        for (Iterator<Adapter> it = clients.iterator(); it.hasNext();) {
+            Adapter a = it.next();
+            if (a.isConnected()) {
+                action.accept(a);
+            } else {
+                it.remove();
+            }
+        }
+    }
+
+    @Override
+    public void onButtonDown(Node node, Pin pin, int upTime) {
+        processEvent(a -> {
+            a.sendPinChange(node, pin, 0);
+        });
+    }
+
+    @Override
+    public void onButtonUp(Node node, Pin pin, int downTime) {
+        processEvent(a -> {
+            a.sendPinChange(node, pin, 1);
+        });
+    }
+
+    @Override
+    public void onReboot(Node node, int pingCounter, int rconValue) throws IOException {
+    }
+    
+    @Override
+    public WebSocketAdapter newAdapter() {
+        return new Adapter();
+    }
+
+    private class Adapter extends AbstractWebSocketAdapter {
+        @Override
+        public void onWebSocketConnect(Session session) {
+            super.onWebSocketConnect(session);
+            // Register itself to parent for events
+            clients.add(this);
+        }
+
+        @Override
+        public void onWebSocketClose(int statusCode, String reason) {
+            // Unregister itself from parent
+            clients.remove(this);
+            super.onWebSocketClose(statusCode, reason);
+        }
+
+        @Override
+        public void onWebSocketText(String message) {
+            super.onWebSocketText(message);
+            // Receives message in format "node.pin"
+            int i = message.indexOf('.');
+            int j = message.indexOf('=');
+            if (i > 0 && j > i) {
+                try {
+                    int id = Integer.parseInt(message.substring(0, i));
+                    Pin pin = Pin.fromString(message.substring(i + 1, j));
+                    String value = message.substring(j + 1);
+                    // TODO: HOWTO detect which pin is PWM?
+                    Node n = nodeInfoRegistry.getNode(id);
+                    if (n != null) {
+                        if (value.endsWith("%")) {
+                            // PWM
+                            int v = Integer.parseInt(value.substring(0, value.length() - 1));
+                            v = v * 48 / 100;
+                            n.setManualPwmValue(pin, v);
+                        } else {
+                            // Value
+                            n.setPinValue(pin, Integer.parseInt(value));
+                        }
+                    }
+                } catch (NumberFormatException e) {
+                } catch (IllegalArgumentException | IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        public void sendPinChange(Node node, Pin pin, int value) {
+            // Something changed - notify
+            JsonWriter w = new JsonWriter(false);
+            w.startObject();
+            w.startArrayAttribute("changes");
+            w.startObject();
+            w.addAttribute("id", node.getNodeId() + "." + pin.getPort() + Integer.toString(pin.getPinIndex()));
+            w.addAttribute("value", Integer.toString(value & 1));
+            w.close();
+            w.close();
+            w.close();
+            sendText(w.toString());
+        }
+    }
+}
