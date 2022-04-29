01e1ddc6ca3e2a59711bd540382a958efdad4829
Pavel Chuchma <Pavel.Chuchma@ataccama.com>
2022-04-29 21:46:32 +0200

    increasing max log file size + error handling in finish-deploy.sh

--- home-controller/script/finish-deploy.sh
+++ home-controller/script/finish-deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 rm -rf /usr/local/bin/homeAutomation/home-controller-0.1.0/
-cd /usr/local/bin/homeAutomation/
+cd /usr/local/bin/homeAutomation/ || exit
 tar -xvf home-controller-0.1.0.tar
 sudo /etc/init.d/homeAutomation restart
