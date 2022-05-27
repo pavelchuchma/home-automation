61085657d8d81e638aec5cc54efea555e7204a08
Pavel Chuchma <Pavel.Chuchma@ataccama.com>
2022-05-27 23:19:04 +0200

    adding name to GenericCodeAction

--- home-controller/controller/src/main/java/org/chuma/homecontroller/controller/action/GenericCodeAction.java
+++ home-controller/controller/src/main/java/org/chuma/homecontroller/controller/action/GenericCodeAction.java
@@ -9,9 +9,20 @@ import org.chuma.homecontroller.controller.actor.Actor;
  */
 public class GenericCodeAction implements Action {
     private final IntConsumer code;
+    private final String name;
 
     public GenericCodeAction(IntConsumer code) {
+        this(code, null);
+    }
+
+    /**
+     *
+     * @param code body of perform() method
+     * @param name name for debug purposes only
+     */
+    public GenericCodeAction(IntConsumer code, String name) {
         this.code = code;
+        this.name = name;
     }
 
     @Override
@@ -25,6 +36,10 @@ public class GenericCodeAction implements Action {
     }
 
     public String toString() {
-        return String.format("%s@%s", getClass().getName(), Integer.toHexString(System.identityHashCode(this)));
+        StringBuilder s = new StringBuilder().append(getClass().getName()).append("@").append(Integer.toHexString(System.identityHashCode(this)));
+        if (name != null) {
+            s.append("(").append(name).append(")");
+        }
+        return s.toString();
     }
 }
