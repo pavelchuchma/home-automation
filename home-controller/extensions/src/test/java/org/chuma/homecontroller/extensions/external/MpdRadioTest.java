24922b5a92224d142d995c51405f2dfb54738d81
Pavel Chuchma <Pavel.Chuchma@ataccama.com>
2022-11-02 23:38:43 +0100

    changing default mpd radio stream

--- home-controller/extensions/src/test/java/org/chuma/homecontroller/extensions/external/MpdRadioTest.java
+++ home-controller/extensions/src/test/java/org/chuma/homecontroller/extensions/external/MpdRadioTest.java
@@ -5,7 +5,8 @@ import org.junit.Assert;
 
 public class MpdRadioTest extends TestCase {
     public void testPlay() {
-        MpdRadio radio = new MpdRadio("192.168.68.150", "http://icecast8.play.cz/cro1-128.mp3");
+        MpdRadio radio = new MpdRadio("192.168.68.150", "http://rozhlas.stream/radiozurnal_mp3_128.mp3");
+//        MpdRadio radio = new MpdRadio("192.168.68.150", "https://rozhlas.stream/radiozurnal_mp3_128.mp3");
         radio.start();
         Assert.assertTrue(radio.isPlaying());
         radio.stop();
