091995a7e15d033cd8afcb2572eef84752fd2a50
Pavel Chuchma <Pavel.Chuchma@ataccama.com>
2024-01-13 17:08:44 +0100

    - build by java11 and run on java17 - new mdp client lib (java17)

--- home-controller/script/deploy-pi.bat
+++ home-controller/script/deploy-pi.bat
@@ -1,7 +1,7 @@
 set host=pi
 set logFile=%temp%\.restart-%host%.log
 if exist %logFile% del %logFile%
-pscp -2 -i N:\Data\cert\sshKey\sshKey.private.ppk %~dp0..\app\build\distributions\home-controller-0.1.0.tar %~dp0runJar-debug.sh pi@%host%:/usr/local/bin/homeAutomation/ || pause
-putty.exe -ssh -2 -i N:\Data\cert\sshKey\sshKey.private.ppk -l pi -sessionlog %logFile% -m "%~dp0finish-deploy.sh" %host%
+"C:\Program Files\PuTTY\pscp" -2 -i N:\Data\cert\sshKey\sshKey.private.ppk %~dp0..\app\build\distributions\home-controller-0.1.0.tar %~dp0runJar-debug.sh pi@%host%:/usr/local/bin/homeAutomation/ || pause
+"C:\Program Files\PuTTY\putty.exe" -ssh -2 -i N:\Data\cert\sshKey\sshKey.private.ppk -l pi -sessionlog %logFile% -m "%~dp0finish-deploy.sh" %host%
 @type %logFile%
 @del %logFile%
