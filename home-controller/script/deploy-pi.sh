745e0d1a44353c21436ba01a4c9aabe1227d71d4
Pavel Chuchma <pavel.chuchma@ataccama.com>
2024-12-22 16:47:00 +0100

    updating deploy script

--- home-controller/script/deploy-pi.sh
+++ home-controller/script/deploy-pi.sh
@@ -1,4 +1,16 @@
-host=pi
+#!/bin/bash
+
+# Get the directory of the script
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+# Configuration
+host=pi.local
 version=0.1.0
-scp ../app/build/distributions/home-controller-$version.tar runJar-debug.sh pi@$host:/usr/local/bin/homeAutomation/ || exit
-ssh -l pi $host < finish-deploy.sh
+
+# Paths
+BUILD_DIR="$SCRIPT_DIR/../app/build/distributions"
+DEPLOY_DIR="/usr/local/bin/homeAutomation"
+
+# Deployment
+scp "$BUILD_DIR/home-controller-$version.tar" "$SCRIPT_DIR/runJar-debug.sh" "pi@$host:$DEPLOY_DIR/" || exit
+ssh -l pi "$host" < "$SCRIPT_DIR/finish-deploy.sh"
