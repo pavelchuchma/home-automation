dde0355941e76a8e2b090b253799e4bce710284e
Pavel Chuchma <pavel.chuchma@ataccama.com>
2026-02-27 06:42:48 +0100

    Add VS Code launch config and deployment script for Raspberry Pi 2

---
+++ home-controller/script/deploy-pi2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Get the directory of the script
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+# Configuration
+host=pi2
+version=0.1.0
+
+# Paths
+BUILD_DIR="$SCRIPT_DIR/../app/build/distributions"
+DEPLOY_DIR="/usr/local/bin/homeAutomation"
+
+# Deployment
+scp "$BUILD_DIR/home-controller-$version.tar" "$SCRIPT_DIR/runJar-debug.sh" "pi@$host:$DEPLOY_DIR/" || exit
+ssh -l pi "$host" < "$SCRIPT_DIR/finish-deploy.sh"
